75134fe1e5e86a48f138b019fd95a39b9266d1f7
Jordan Foreman <thejordanforeman@gmail.com>
2012-06-19 10:39:15 -0500

    Initial Commit…as in just getting started…as in new…

---
+++ header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Jordan Foreman</title>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	</head>
