5a7388411ed266d3c0914ec99266b223f58cd336
Jordan Foreman <thejordanforeman@gmail.com>
2012-06-22 23:10:46 -0500

    Quick edit from home
    Didn't really do anything. Added a few files, and added a CSS reset

--- header.php
+++ header.php
@@ -5,5 +5,6 @@
 		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 		<link rel="stylesheet" href="css/styles.css" />
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script type="text/javascript" src="js/scripts.js"></script>
 	</head>
 	<body>
